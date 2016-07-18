2beb5c8f3efe024dcd59481d3203113060d38053
Jeon <hyeon454@naver.com>
2016-07-18 13:12:12 +0900

    bookflip

---
+++ src/main/java/com/dava/myapp/hyun/ReadBookController.java
@@ -0,0 +1,19 @@
+package com.dava.myapp.hyun;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class ReadBookController {
+
+	@RequestMapping(value = "/readbook/read", method = RequestMethod.GET)
+	public String home() {
+
+		return "/readbook/readbook";
+	}
+	
+}
